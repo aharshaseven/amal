6dea7a85a82782ac5dc50c12ad3b4136e417db1d
aharshaseven <aharsha0709@gmail.com>
2026-02-01 22:59:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
 const yes = document.getElementById("yes");
 const no = document.getElementById("no");
-const result = document.querySelector(".result");
+const reveal = document.querySelector(".reveal");
+const stickers = document.querySelector(".stickers");
 
 yes.onclick = () => {
-  result.style.display = "block";
+  reveal.style.display = "block";
 };
 
+/* NO button chaos */
+
 no.onmouseover = () => {
-  no.style.position = "absolute";
-  no.style.left = Math.random()*70 + "%";
-  no.style.top = Math.random()*70 + "%";
+  no.style.position="absolute";
+  no.style.left=Math.random()*80+"vw";
+  no.style.top=Math.random()*80+"vh";
 };
+
+/* knitted patch style dogs */
+
+const dogs = [
+ "https://i.imgur.com/JbK0ZbH.png",
+ "https://i.imgur.com/6KQ6kXK.png",
+ "https://i.imgur.com/7xkQZgR.png",
+ "https://i.imgur.com/NwzX9ZB.png"
+];
+
+setInterval(()=>{
+  const d=document.createElement("img");
+  d.src=dogs[Math.floor(Math.random()*dogs.length)];
+  d.style.left=Math.random()*100+"vw";
+  d.style.animationDuration=10+Math.random()*8+"s";
+  stickers.appendChild(d);
+  setTimeout(()=>d.remove(),18000);
+},900);
